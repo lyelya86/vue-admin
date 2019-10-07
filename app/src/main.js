f6ab51b14552460f354141959953c46381c6ed40
lyelya86 <lyelya_86@mail.ru>
2019-10-07 16:00:10 +0300

    lesson3

--- app/src/main.js
+++ app/src/main.js
@@ -1,1 +1,57 @@
-const $ = require("jquery");
+// const $ = require("jquery");
+
+// function getPagesList() {
+//     $("h1").remove();
+//     $.get("./api", (data) => {
+//         data.forEach((file) => {
+//             $("body").append("<h1>" + file + "</h1>");
+//         });
+//     }, "JSON");
+// }
+// getPagesList() ;
+
+// $("button").click(() => {
+//     $.post("./api/createNewHtmlPage.php", {
+//         "name": $("input").val()
+//     }, (data) => {
+//         getPagesList();
+//     })
+//     .fail(() => {
+//         alert("такая страница уже существует");
+//     })
+// })
+
+
+
+const Vue = require("vue");
+const axios = require("axios");
+
+new Vue ({
+    el: "#app",
+    data: {
+        "pageList": [],
+        "newPageName": ""
+    },
+    methods: {
+        createPage() {
+            axios
+                .post("./api/createNewHtmlPage.php", { "name": this.newPageName })
+                .then(() => this.updatePageList())
+        },
+        updatePageList() {
+            axios
+                .get("./api/")
+                .then((response) => {
+                    this.pageList = response.data
+            })
+        },
+        deletePage(page) {
+            axios
+                .post("./api/deletePage.php", { "name": page })
+                .then(() => this.updatePageList())
+        }
+    },
+    created() {
+        this.updatePageList();
+    }
+})
--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ gulp.task("build-sass", () => {
 
 gulp.task("copy-api", () => {
     return gulp.src("./app/api/**/*.*")
-                .pipe(gulp.dest(dist + "/app"));
+                .pipe(gulp.dest(dist + "/api"));
 });
 
 gulp.task("copy-assets", () => {
